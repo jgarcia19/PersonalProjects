1c6a78f35773cf2bb5c82b890a190fe498389e4d
jgarcia19 <jasong2000@gmail.com>
2023-09-02 13:51:29 -0400

    Added another coding problem

--- random.java
+++ random.java
@@ -1,14 +1,54 @@
 public class random {
     public static void main(String[] args) {
-        String s = "A";
-        int numRows = 1;
+        String test = "(*)";
 
-        String result = convert(s, numRows);
+        System.out.println(isBalanced(test));
+    }
+
+    /**
+     * You're given a string consisting solely of (, ), and *. * can represent either a (, ), or an empty string. Determine whether the parentheses are balanced.
+     * For example, (()* and (*) are balanced. )*( is not balanced.
+     * 
+     * @param s
+     * @return 
+     */
+
+    public static boolean isBalanced(String s) {
+        return isBalancedRec(s, 0, 0);
+    }
+
+    private static boolean isBalancedRec(String s, int index, int stack) {
+        if (s.length() == index) {
+            if (stack == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if (stack < 0) {
+            return false;
+        }
+        if (s.charAt(index) == '(') {
+            return isBalancedRec(s, index + 1, stack + 1);
+
+        } else if (s.charAt(index) == ')') {
+            return isBalancedRec(s, index + 1, stack - 1);
+        } else {
+            String s1 = s.substring(0, index) + "(" + s.substring(index + 1);
+            String s2 = s.substring(0, index) + ")" + s.substring(index + 1);
+            String s3 = s.substring(0, index) + s.substring(index + 1);
+            return isBalancedRec(s1, index, stack) || isBalancedRec(s2, index, stack) || isBalancedRec(s3, index, stack); 
+        }
     }
 
     public static String convert(String s, int numRows) {
         String returnStr = "";
         int interval = (numRows - 1) * 2;
+
+        if (numRows == 1) {
+            return s;
+        }
 
         for (int i = 0; i < numRows; i++) {
             boolean first = true;
