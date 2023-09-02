e91b0879f01d2a8cd09d744bbedb585117ba3cc1
jgarcia19 <jasong2000@gmail.com>
2023-09-01 22:59:16 -0400

    Added a leetcode problem

--- random.java
+++ random.java
@@ -1,13 +1,37 @@
 public class random {
     public static void main(String[] args) {
+        String s = "A";
+        int numRows = 1;
 
-        try {
-            new int[]{1,2,3};
+        String result = convert(s, numRows);
+    }
 
-        } catch (Exception e) {
-            System.out.println("Exception was caught!");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Index Exception was caught!");
+    public static String convert(String s, int numRows) {
+        String returnStr = "";
+        int interval = (numRows - 1) * 2;
+
+        for (int i = 0; i < numRows; i++) {
+            boolean first = true;
+            int firstJump = interval - (i * 2);
+            int secondJump = i * 2;
+            int j = i;
+            while (j < s.length()) {
+                System.out.println(j);
+                if (first) {
+                    if (firstJump != 0) {
+                        returnStr = returnStr + s.charAt(j);
+                        j = j + firstJump;
+                    }
+                    first = false;
+                } else {
+                    if (secondJump != 0) {
+                        returnStr = returnStr + s.charAt(j);
+                        j = j + secondJump;
+                    }
+                    first = true;
+                }
+            }
         }
+        return returnStr;
     }
 }
